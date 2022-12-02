54896dd99607a5ca73550f186ac7860b2a42bfe7
mploythai <110638329+mploythai@users.noreply.github.com>
2022-12-01 23:05:13 -0800

    created modules to generate cards for team page

--- dist/index.js
+++ dist/index.js
@@ -1,77 +1,87 @@
-const rock = document.querySelector("#Rock");
-const scissor = document.querySelector("#Scissor");
-const paper = document.querySelector("#Rock");
+import toggleTeam from "./modules/toggle-team.js";
+import { createCards } from "./modules/team-cards.js";
 
-const playerScore = 0;
-const computerScore = 0;
-const roundNumber = 0;
+const team = document.getElementById("team");
+createCards();
 
-rock.addEventListener("click", () => {
-	// make selection for first player
-	let player = "rock";
-	let computer = selectionAI();
-	determineWinner(player, computer);
-	updateRound();
+team.addEventListener("click", () => {
+  toggleTeam();
 });
 
-paper.addEventListener("click", () => {
-	// make selection for first player
-	let player = "paper";
-	let computer = selectionAI();
-	determineWinner(player, computer);
-	updateRound();
-});
+// const rock = document.querySelector("#Rock");
+// const scissor = document.querySelector("#Scissor");
+// const paper = document.querySelector("#Rock");
 
-scissor.addEventListener("click", () => {
-	// make selection for first player
-	let player = "scissor";
-	let computer = selectionAI();
-	determineWinner(player, computer);
-	updateRound();
-});
+// const playerScore = 0;
+// const computerScore = 0;
+// const roundNumber = 0;
 
-function selectionAI() {
-	// randomize function here
-	let randomNum = Math.floor(Math.random() * 3) + 1;
-	switch (randomNum) {
-		//took out quotes
-		case 0:
-			return "rock";
-		case 1:
-			return "paper";
-		case 2:
-			return "scissor";
-	}
-}
+// rock.addEventListener("click", () => {
+//   // make selection for first player
+//   let player = "rock";
+//   let computer = selectionAI();
+//   determineWinner(player, computer);
+//   updateRound();
+// });
 
-function determineWinner(player, computer) {
-	// add code here
-	if (player === computer) {
-		//return tie
-		return "It's a Tie"
-	} else if (player === "scissor" && computer === "paper") {
-		// return player wins
-		updateScore();
-		return "You Won"
-	} else if (player === "scissor" && computer === "rock") {
-		// return computer wins
-		updateScore();
-		return "You Lost"
-	} else if (player === "paper" && computer === "scissor") {
-		// return computer wins
-		updateScore();
-		return "You Lost"
-	} else if (player === "paper" && computer === "rock") {
-		// return player wins
-		updateScore();
-		return "You Won"
-	} else if (player === "rock" && computer === "paper") {
-		// return computer wins
-		updateScore();
-		return "You Lost"
-	} else if (player === "rock" && computer === "scissor") {
-		// return player wins
-		updateScore();
-		return "You Won"
-	}
-}
+// paper.addEventListener("click", () => {
+//   // make selection for first player
+//   let player = "paper";
+//   let computer = selectionAI();
+//   determineWinner(player, computer);
+//   updateRound();
+// });
+
+// scissor.addEventListener("click", () => {
+//   // make selection for first player
+//   let player = "scissor";
+//   let computer = selectionAI();
+//   determineWinner(player, computer);
+//   updateRound();
+// });
+
+// function selectionAI() {
+//   // randomize function here
+//   let randomNum = Math.floor(Math.random() * 3) + 1;
+//   switch (randomNum) {
+//     //took out quotes
+//     case 0:
+//       return "rock";
+//     case 1:
+//       return "paper";
+//     case 2:
+//       return "scissor";
+//   }
+// }
+
+// function determineWinner(player, computer) {
+//   // add code here
+//   if (player === computer) {
+//     //return tie
+//     return "It's a Tie";
+//   } else if (player === "scissor" && computer === "paper") {
+//     // return player wins
+//     updateScore();
+//     return "You Won";
+//   } else if (player === "scissor" && computer === "rock") {
+//     // return computer wins
+//     updateScore();
+//     return "You Lost";
+//   } else if (player === "paper" && computer === "scissor") {
+//     // return computer wins
+//     updateScore();
+//     return "You Lost";
+//   } else if (player === "paper" && computer === "rock") {
+//     // return player wins
+//     updateScore();
+//     return "You Won";
+//   } else if (player === "rock" && computer === "paper") {
+//     // return computer wins
+//     updateScore();
+//     return "You Lost";
+//   } else if (player === "rock" && computer === "scissor") {
+//     // return player wins
+//     updateScore();
+//     return "You Won";
+//   }
+// }
