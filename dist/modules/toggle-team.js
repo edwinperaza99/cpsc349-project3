fa9493f23b8aa39064a9f9853f616f8dba91b8c4
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2022-12-03 18:26:55 -0800

    delete dis

--- dist/modules/toggle-team.js
+++
@@ -1,7 +0,0 @@
-export default function toggleTeam() {
-  const home = document.getElementById("home");
-  const cards = document.getElementById("cards");
-
-  home.classList.add("hidden");
-  cards.classList.remove("hidden");
-}
